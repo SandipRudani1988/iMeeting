25e0135b601e67ca6f17d7126f1e6897124e5539
Dhara Shah <dshah4289@gmail.com>
2016-11-20 23:17:57 -0500

    no message

---
+++ iMeetin/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  iMeetin
+//
+//  Created by Dhara Shah on 2016-11-20.
+//  Copyright © 2016 sandip Rudani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ iMeetin/DetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.m
+//  iMeetin
+//
+//  Created by Dhara Shah on 2016-11-20.
+//  Copyright © 2016 sandip Rudani. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@implementation DetailViewController
+
+@end
