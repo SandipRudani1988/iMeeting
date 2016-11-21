82245631614dcfdfbdf3401c49dfa11aa5cf77af
sandip.rudani <sandip.rudani@prodigylabs.net>
2016-11-20 22:40:40 -0500

    Initial commit

---
+++ iMeetin/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iMeetin
+//
+//  Created by sandip Rudani on 2016-11-20.
+//  Copyright © 2016 sandip Rudani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
